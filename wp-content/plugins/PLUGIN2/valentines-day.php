47e2d2aa50ef63dcd86600f449ce0cf10f041f4d
daniiltrutnevikt <trutnev.daniil@khk.ee>
2020-12-18 14:03:02 +0200

    modified plugin `PLUGIN2`

---
+++ wp-content/plugins/PLUGIN2/valentines-day.php
@@ -0,0 +1,22 @@
+
+<?php
+/*
+Plugin Name: My First plugin
+Plugin URI: https://daniiltrutnev.ikt.khk.ee/wordpress/
+Description: This is my first WordPress Plugin
+Author: Daniil 
+Author URI: https://daniiltrutnev.ikt.khk.ee/wordpress/
+Version: 1.0
+*/
+
+
+define( 'DC_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+
+//functions for plugin administration
+include( 'includes/developer.php' );
+
+//create & manage plugin settings
+include( 'includes/settings.php' );
+
+//holds the "heart" of the plugin (sorry!)
+include( 'includes/snowflake.php' );
